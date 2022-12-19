58589dc360fc471711f9f2b7612f9100b7ca5f1f
adesola2003 <maryolasehinde20@gmail.com>
2022-12-19 14:07:19 +0100

    Update

--- Pages/AgendaPage.xaml.cs
+++ Pages/AgendaPage.xaml.cs
@@ -1,9 +1,37 @@
+using My_Confrence_App.ViewModels;
+
 namespace My_Confrence_App.Pages;
 
+
+
+[QueryProperty(nameof(Day), nameof(Day))]
 public partial class AgendaPage : ContentPage
 {
-	public AgendaPage()
-	{
-		InitializeComponent();
-	}
+    readonly ScheduleViewModel vm;
+    public static int Day { get; set; }
+
+    public AgendaPage(ScheduleViewModel vm)
+    {
+        InitializeComponent();
+
+        Day++;
+        vm.Day = Day;
+
+
+        BindingContext = this.vm = vm;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (vm.Schedule.Count == 0)
+            await vm.LoadDataCommand.ExecuteAsync(null);
+    }
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+
+
+    }
 }
